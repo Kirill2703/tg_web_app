786b6e26c92ed380cee0af0c87691ac9d7b3b33e
Kirill <wondersweb75@gmail.com>
2024-09-11 15:23:09 +0200

    header+button

---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+
+    const onClose = () => {
+      tg.close();
+    };
+    return (
+        <div className={'header'}>
+            <button onClick={onClose}>Close</button>
+            <span className={'username'}>{tg.initDataUnsef?.user?.username }</span>
+        </div>
+    );
+}
+
+export default Header;
