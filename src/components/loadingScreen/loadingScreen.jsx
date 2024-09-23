f3714eb9e6386699b25b17ade6c7f70e39656499
Kirill <wondersweb75@gmail.com>
2024-09-23 13:00:48 +0200

    state

--- src/components/loadingScreen/loadingScreen.jsx
+++ src/components/loadingScreen/loadingScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const LoadingScreen = () => {
+    const username = useSelector((state) => state.user.username);
     return (
       <div className="loading-screen">
         <div>
-            <img src="/loading-screen.jpg" alt="Loading" />
-            <h2 className='text-loading-screen'>Загрузка...</h2>
+          <img src="/loading-screen.jpg" alt="Loading" />
+          <h2 className="text-loading-screen">Загрузка...</h2>
+          {username && <p>Добро пожаловать, {username}!</p>}
         </div>
       </div>
     );
