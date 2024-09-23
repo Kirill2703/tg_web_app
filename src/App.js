561ec1d82f1be2e73d007372539d29c9e25bea33
Kirill <wondersweb75@gmail.com>
2024-09-23 11:28:01 +0200

    loading+screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/header/header';
+import LoadingScreen from './components/loadingScreen/loadingScreen';
 const tg = window.Telegram.WebApp;
 
 function App() {
+  const [loading, setLoading] = useState(true); // Добавьте состояние loading
 
   useEffect(() => {
-    tg.ready()
-  })
+    // Симуляция загрузки, замените это на ваши реальные данные
+    setTimeout(() => {
+      setLoading(false);
+      tg.ready(); // Подготовьте Telegram Web App
+    }, 2000); // 2 секунды
+  }, [tg]);
 
-  
+  if (loading) {
+    return <LoadingScreen />; // Показываем компонент загрузки
+  }
+
   return (
     <div>
       <Header />
--- src/components/loadingScreen/loadingScreen.jsx
+++ src/components/loadingScreen/loadingScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingScreen = () => {
     return (
       <div className="loading-screen">
-        <img src="../images/loading-image.jpg" alt="Loading" />
+        <img src="/loading-screen.jpeg" alt="Loading" />
         <h2>Загрузка...</h2>
       </div>
     );
