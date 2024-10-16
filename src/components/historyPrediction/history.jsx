9de45657d0ecdbef2a2ce6d925bf14f787c81e7f
Kirill <wondersweb75@gmail.com>
2024-10-16 21:33:24 +0200

    check+history

--- src/components/historyPrediction/history.jsx
+++ src/components/historyPrediction/history.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUserHistory } from "../../thunks/historyPredictionThunk";
 import LoadingScreen from "../loadingScreen/loadingScreen";
+import { fetchUserNameByChatId } from "../../thunks/userThunk";
 
 const History = () => {
   const dispatch = useDispatch();
@@ -10,46 +11,62 @@ const History = () => {
   const [error, setError] = useState(null);
   const [history, setHistory] = useState([]);
 
-  useEffect(() => {
-    const fetchHistory = async () => {
-      if (currentUser) {
-        setLoading(true);
-        try {
-          const result = await dispatch(fetchUserHistory(currentUser.username));
-          if (fetchUserHistory.fulfilled.match(result)) {
-            setHistory(result.payload);
-          } else {
-            setError(result.error.message);
-          }
-        } catch (err) {
-          setError(err.message);
-        } finally {
-          setLoading(false); // Убедитесь, что это всегда вызывается
-        }
-      } else {
-        setLoading(false); // Если текущего пользователя нет, завершите загрузку
-      }
-        
-        fetchHistory()
-    };
-  }, [dispatch, currentUser]);
-    
-    if (loading) return <LoadingScreen />
-    if (error) return <div>ERROR</div>
+  const tg = window.Telegram.WebApp;
+  const chatId = tg.initDataUnsafe?.user?.id;
 
-    return <div>
-        <h1>История прогнозов</h1>
-        {history.length === 0 ? 
-            (<p>У вас ещё не было прогнозов</p>) : (
-                <ul>
-                    {history.map((item) => (
-                        <li key={item._id}>
-                            <p>{item.match} - Твой прогноз: {item.selectedTeam} | Результат: {item.result }</p>
-                        </li>
-                    ))}
-                </ul>
-        ) }
-  </div>;
+  useEffect(() => {
+    if (chatId) {
+      dispatch(fetchUserNameByChatId);
+    }
+  }, []);
+
+  //   useEffect(() => {
+  //     const fetchHistory = async () => {
+  //       if (currentUser) {
+  //         setLoading(true);
+  //         try {
+  //           const result = await dispatch(fetchUserHistory(currentUser.username));
+  //           if (fetchUserHistory.fulfilled.match(result)) {
+  //             setHistory(result.payload);
+  //           } else {
+  //             setError(result.error.message);
+  //           }
+  //         } catch (err) {
+  //           setError(err.message);
+  //         } finally {
+  //           setLoading(false); // Убедитесь, что это всегда вызывается
+  //         }
+  //       } else {
+  //         setLoading(false); // Если текущего пользователя нет, завершите загрузку
+  //       }
+
+  //         fetchHistory()
+  //     };
+  //   }, [dispatch, currentUser]);
+
+  // if (loading) return <LoadingScreen />
+  // if (error) return <div>ERROR</div>
+
+  return (
+    <div>
+      {currentUser.username}
+      <h1>История прогнозов</h1>
+      {history.length === 0 ? (
+        <p>У вас ещё не было прогнозов</p>
+      ) : (
+        <ul>
+          {history.map((item) => (
+            <li key={item._id}>
+              <p>
+                {item.match} - Твой прогноз: {item.selectedTeam} | Результат:{" "}
+                {item.result}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default History;
