dcd01fd68d2e4c41da9552ecd7fc603d12a41fa4
Kirill <wondersweb75@gmail.com>
2024-10-23 23:51:51 +0200

    check+main+component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,5 +1,55 @@
+// import React, { useEffect, useState } from "react";
+// import { fetchAllUsers, fetchUserNameByChatId } from "../../thunks/userThunk";
+// import { useDispatch, useSelector } from "react-redux";
+// import LoadingScreen from "../loadingScreen/loadingScreen";
+// import "../../App.css";
+// import { Link } from "react-router-dom";
+
+// const Main = ({ setLoading }) => {
+//   const dispatch = useDispatch();
+//   const currentUser = useSelector((state) => state.user.currentUser);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const tg = window.Telegram.WebApp;
+//   const chatId = tg.initDataUnsafe?.user?.id;
+
+//   useEffect(() => {
+//     setLoading(true);
+
+//     const loadingTimer = setTimeout(() => {
+//       setIsLoading(false);
+//       setLoading(false);
+//     }, 2000);
+
+//     if (chatId) {
+//       const fetchUserTimer = setTimeout(() => {
+//         dispatch(fetchUserNameByChatId(chatId));
+//       }, 2000);
+
+//       return () => clearTimeout(fetchUserTimer);
+//     }
+
+//     return () => clearTimeout(loadingTimer);
+//   }, [dispatch, chatId]);
+
+//   if (isLoading) {
+//     return <LoadingScreen />;
+//   }
+
+//   return (
+//     <div>
+//       <button>
+//         <Link to="/history">History</Link>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Main;
+
+
 import React, { useEffect, useState } from "react";
-import { fetchAllUsers, fetchUserNameByChatId } from "../../thunks/userThunk";
+import { fetchUserNameByChatId } from "../../thunks/userThunk";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingScreen from "../loadingScreen/loadingScreen";
 import "../../App.css";
@@ -36,10 +86,22 @@ const Main = ({ setLoading }) => {
     return <LoadingScreen />;
   }
 
+  // Проверка, есть ли текущий пользователь
+  if (!currentUser) {
+    return <div>Пользователь не найден</div>;
+  }
+
+  // Подсчет пройденных квизов
+  const completedQuizzesCount = currentUser.completedQuizzes.length; // Количество пройденных квизов
+
   return (
     <div>
+      <h1>Добро пожаловать, {currentUser.username}!</h1>
+      <p>Общее количество очков: {currentUser.points || 0}</p>
+      <p>Количество пройденных квизов: {completedQuizzesCount}</p>
+
       <button>
-        <Link to="/history">History</Link>
+        <Link to="/history">История</Link>
       </button>
     </div>
   );
