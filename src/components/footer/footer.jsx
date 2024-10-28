ad5c834ca10d92a216c8e84bc0057120b8d88607
Kirill <wondersweb75@gmail.com>
2024-10-28 20:55:03 +0100

    css+footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,37 +6,39 @@ import { FaBullseye, FaHome, FaRegLightbulb, FaTrophy } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="nav-menu">
-        <Link to="/prediction" className="link-nav-menu">
-          <div className="icon-text-container">
-            <FaBullseye className="icon" />
-            <span>Прогнозы</span>
-          </div>
-        </Link>
-      </div>
-      <div className="nav-menu">
-        <Link to="/quiz" className="link-nav-menu">
-          <div className="icon-text-container">
-            <FaRegLightbulb className="icon" />
-            <span>Викторина</span>
-          </div>
-        </Link>
-      </div>
-      <div className="nav-menu">
-        <Link to="/" className="link-nav-menu">
-          <div className="icon-text-container">
-            <FaHome className="icon" />
-            <span>Дом</span>
-          </div>
-        </Link>
-      </div>
-      <div className="nav-menu">
-        <Link to="/table" className="link-nav-menu">
-          <div className="icon-text-container">
-            <FaTrophy className="icon" />
-            <span>Таблица</span>
-          </div>
-        </Link>
+      <div className="container">
+        <div className="nav-menu">
+          <Link to="/prediction" className="link-nav-menu">
+            <div className="icon-text-container">
+              <FaBullseye className="icon" />
+              <span>Прогнозы</span>
+            </div>
+          </Link>
+        </div>
+        <div className="nav-menu">
+          <Link to="/quiz" className="link-nav-menu">
+            <div className="icon-text-container">
+              <FaRegLightbulb className="icon" />
+              <span>Викторина</span>
+            </div>
+          </Link>
+        </div>
+        <div className="nav-menu">
+          <Link to="/" className="link-nav-menu">
+            <div className="icon-text-container">
+              <FaHome className="icon" />
+              <span>Дом</span>
+            </div>
+          </Link>
+        </div>
+        <div className="nav-menu">
+          <Link to="/table" className="link-nav-menu">
+            <div className="icon-text-container">
+              <FaTrophy className="icon" />
+              <span>Таблица</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
