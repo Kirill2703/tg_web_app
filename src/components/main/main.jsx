1fcf7fac2a8d39f82943a6f45f1627f19b8f56a4
Kirill <wondersweb75@gmail.com>
2024-11-02 21:06:13 +0100

    without-backround

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,51 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { fetchAllUsers, fetchUserNameByChatId } from "../../thunks/userThunk";
-// import { useDispatch, useSelector } from "react-redux";
-// import LoadingScreen from "../loadingScreen/loadingScreen";
-// import "../../App.css";
-// import { Link } from "react-router-dom";
-
-// const Main = ({ setLoading }) => {
-//   const dispatch = useDispatch();
-//   const currentUser = useSelector((state) => state.user.currentUser);
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   const tg = window.Telegram.WebApp;
-//   const chatId = tg.initDataUnsafe?.user?.id;
-
-//   useEffect(() => {
-//     setLoading(true);
-
-//     const loadingTimer = setTimeout(() => {
-//       setIsLoading(false);
-//       setLoading(false);
-//     }, 2000);
-
-//     if (chatId) {
-//       const fetchUserTimer = setTimeout(() => {
-//         dispatch(fetchUserNameByChatId(chatId));
-//       }, 2000);
-
-//       return () => clearTimeout(fetchUserTimer);
-//     }
-
-//     return () => clearTimeout(loadingTimer);
-//   }, [dispatch, chatId]);
-
-//   if (isLoading) {
-//     return <LoadingScreen />;
-//   }
-
-//   return (
-//     <div>
-//       <button>
-//         <Link to="/history">History</Link>
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Main;
 
 import React, { useEffect, useState } from "react";
 import { fetchUserNameByChatId } from "../../thunks/userThunk";
@@ -94,67 +46,63 @@ const Main = ({ setLoading }) => {
   const completedQuizzesCount = currentUser.completedQuizzes.length; // Количество пройденных квизов
 
   return (
-    <>
-      <div style={{ margin: "12px 28px", position: "relative" }}>
-        <h1 className="username-main-page">
-          Привет, {currentUser.username}&#128075;
-        </h1>
-        {/* <h1>Добро пожаловать, {currentUser.username}!</h1>
+    <div style={{ margin: "12px 28px", position: "relative" }}>
+      <h1 className="username-main-page">
+        Привет, {currentUser.username}&#128075;
+      </h1>
+      {/* <h1>Добро пожаловать, {currentUser.username}!</h1>
       <p>Общее количество очков: {currentUser.points || 0}</p>
       <p>Количество пройденных квизов: {completedQuizzesCount}</p> */}
 
-        <div
-          style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <p className="points-main-page">{currentUser.points}</p>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              margin: "24px 0px 12px -4px",
-            }}
-          >
-            <p className="current-quiz-main-page">{completedQuizzesCount}</p>
-          </div>
-        </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "20px",
+          justifyContent: "center",
+          marginTop: "20px",
+        }}
+      >
+        <button className="btn-link-main-page">
+          <Link to="/rules" className="link-list-mp">
+            Правила
+          </Link>
+        </button>
+        <button className="btn-link-main-page">
+          <Link to="/history" className="link-list-mp">
+            История
+          </Link>
+        </button>
+        <button className="btn-link-main-page">
+          <Link to="/leaders" className="link-list-mp">
+            Таблица лидеров
+          </Link>
+        </button>
+      </div>
 
+      <div style={{ display: "flex", flexDirection: "row", marginTop: "20px" }}>
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            gap: "20px",
-            justifyContent: "center",
-            marginTop: "20px",
+            alignItems: "center",
           }}
         >
-          <button className="btn-link-main-page">
-            <Link to="/rules" className="link-list-mp">
-              Правила
-            </Link>
-          </button>
-          <button className="btn-link-main-page">
-            <Link to="/history" className="link-list-mp">
-              История
-            </Link>
-          </button>
-          <button className="btn-link-main-page">
-            <Link to="/leaders" className="link-list-mp">
-              Таблица лидеров
-            </Link>
-          </button>
+          <p className="points-main-page">{currentUser.points}</p>
         </div>
-
-        {/* <img style={{position: "absolute", left: "-100px", bottom: "200px"}} src="/ball-bckg.png" alt="" /> */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            margin: "24px 0px 12px -4px",
+          }}
+        >
+          <p className="current-quiz-main-page">{completedQuizzesCount}</p>
+        </div>
       </div>
-      <div className="bckg-ball-img"></div>
-    </>
+
+      {/* <img style={{position: "absolute", left: "-100px", bottom: "200px"}} src="/ball-bckg.png" alt="" /> */}
+    </div>
+    // <div className="bckg-ball-img"></div>
   );
 };
 
