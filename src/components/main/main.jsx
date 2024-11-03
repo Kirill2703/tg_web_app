cf6bba27237ae0982fcc388be1c39349a112c85a
Kirill <wondersweb75@gmail.com>
2024-11-03 23:27:16 +0100

    color+links+bckg+img

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -45,83 +45,81 @@ const Main = ({ setLoading }) => {
   const completedQuizzesCount = currentUser.completedQuizzes.length; // Количество пройденных квизов
 
   return (
-    <div style={{ margin: "20px 20px 0 20px", position: "relative" }}>
-      <h1 className="username-main-page">
-        Привет, {currentUser.username}&#128075;
-      </h1>
-      {/* <h1>Добро пожаловать, {currentUser.username}!</h1>
-      <p>Общее количество очков: {currentUser.points || 0}</p>
-      <p>Количество пройденных квизов: {completedQuizzesCount}</p> */}
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          gap: "10px",
-          marginTop: "20px",
-        }}
-      >
-        <button className="btn-link-main-page">
-          <Link to="/rules" className="link-list-mp">
-            Правила Footwise
-          </Link>
-        </button>
-        <button className="btn-link-main-page">
-          <Link to="/history" className="link-list-mp">
-            История прогнозов
-          </Link>
-        </button>
-        <button className="btn-link-main-page">
-          <Link to="/leaders" className="link-list-mp">
-            Таблица лидеров
-          </Link>
-        </button>
-      </div>
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          marginTop: "20px",
-          justifyContent: "center",
-        }}
-      >
+    <>
+      <div style={{ margin: "20px 20px 0 20px", position: "relative" }}>
+        <h1 className="username-main-page">
+          Привет, {currentUser.username}&#128075;
+        </h1>
+        {/* <h1>Добро пожаловать, {currentUser.username}!</h1>
+        <p>Общее количество очков: {currentUser.points || 0}</p>
+        <p>Количество пройденных квизов: {completedQuizzesCount}</p> */}
         <div
           style={{
-            position: "ablosute",
             display: "flex",
+            flexDirection: "row",
             justifyContent: "center",
-            bottom: "100px",
-            width: "160px",
-            height: "160px",
+            gap: "10px",
+            marginTop: "20px",
           }}
         >
-          <img s src="/border-points.png" alt="" />
+          <button className="btn-link-main-page">
+            <Link to="/rules" className="link-list-mp">
+              Правила Footwise
+            </Link>
+          </button>
+          <button className="btn-link-main-page">
+            <Link to="/history" className="link-list-mp">
+              История прогнозов
+            </Link>
+          </button>
+          <button className="btn-link-main-page">
+            <Link to="/leaders" className="link-list-mp">
+              Таблица лидеров
+            </Link>
+          </button>
         </div>
         <div
           style={{
             display: "flex",
+            flexDirection: "row",
+            marginTop: "20px",
             justifyContent: "center",
           }}
         >
-          <p className="points-main-page">{currentUser.points}</p>
+          <div
+            style={{
+              position: "ablosute",
+              display: "flex",
+              justifyContent: "center",
+              bottom: "100px",
+              width: "160px",
+              height: "160px",
+            }}
+          >
+            <img s src="/border-points.png" alt="" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <p className="points-main-page">{currentUser.points}</p>
+          </div>
+          {/* <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              margin: "24px 0px 12px -4px",
+            }}
+          >
+            <p className="current-quiz-main-page">{completedQuizzesCount}</p>
+          </div> */}
         </div>
-
-        {/* <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            margin: "24px 0px 12px -4px",
-          }}
-        >
-          <p className="current-quiz-main-page">{completedQuizzesCount}</p>
-        </div> */}
+        {/* <img style={{position: "absolute", left: "-100px", bottom: "200px"}} src="/ball-bckg.png" alt="" /> */}
       </div>
-      <p>dfsdfsf</p>
-
-      {/* <img style={{position: "absolute", left: "-100px", bottom: "200px"}} src="/ball-bckg.png" alt="" /> */}
-    </div>
+      <p>dsfs</p>
+    </>
     // <div className="bckg-ball-img"></div>
   );
 };
