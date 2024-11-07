d98ca5d7b0198f4225df60f9303ea7b93631803e
Kirill <wondersweb75@gmail.com>
2024-11-07 23:00:08 +0100

    marginsprediction+page

--- src/components/prediction/prediction.jsx
+++ src/components/prediction/prediction.jsx
@@ -3,7 +3,7 @@ import AllLeagues from '../leagues/allLeagues';
 
 const Prediction = () => {
     return (
-        <div style={{margin: "20px 20px 0 20px"}}>
+        <div style={{margin: "0 20px"}}>
             <h2 className='title-prediction'>Choose league:</h2>
             <AllLeagues />
         </div>
