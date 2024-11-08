01f3dab706d06c78cdbe56b9c90204a3dae186e1
Kirill <wondersweb75@gmail.com>
2024-11-08 20:48:20 +0100

    page+quiz+css

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -21,18 +21,60 @@ const Quiz = () => {
     return <div>Ошибка: {error}</div>;
   }
 
-    return (
-      <div>
-        <h1>Квизы</h1>
-        <ul>
-          {quizes.map((quiz) => (
-            <li key={quiz._id}>
-              <Link to={`/quiz/${quiz._id}`}>{quiz.title}</Link>
-            </li>
-          ))}
-        </ul>
+  return (
+    <>
+      <div style={{ margin: "0 20px" }}>
+        <h1 className="header-quizes">Квизы</h1>
+        {quizes.map((quiz) => (
+          <div className="container-card-quiz">
+            <Link
+              to={`/quiz/${quiz._id}`}
+              key={quiz._id}
+              style={{ textDecoration: "none" }}
+            >
+              <div className="card-quiz">
+                <img
+                  src={require("../../icons/england.svg").default}
+                  alt="England"
+                  width={40}
+                  height={40}
+                />
+                <p className="quiz-title">{quiz.title}</p>
+                <div className="quantity-quiz">
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <p className="quantity-points">Награда:</p>
+                    <p className="quantity-points">{quiz.quantityPoints}</p>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <p className="quantity-questions">
+                      Кол-во вопросов:
+                    </p>
+                    <p className="quantity-questions">
+                      {quiz.quantityQuestions}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
-    );
+    </>
+  );
 };
 
 export default Quiz;
