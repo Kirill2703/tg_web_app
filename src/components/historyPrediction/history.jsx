bedbc7cbdc292704a36b45024e7447773e91b0b7
Kirill <wondersweb75@gmail.com>
2024-11-18 23:45:05 +0100

    check+logs

--- src/components/historyPrediction/history.jsx
+++ src/components/historyPrediction/history.jsx
@@ -168,81 +168,61 @@ const History = () => {
      });
   });
   return (
-    <div>
-      <h1 className="history-prediction">History Prediction</h1>
-      {history.length === 0 ? (
-        <p className="make-prediction">
-          You haven`t made any predictions yet. Make your first prediction!
-        </p>
-      ) : (
-        <div className="history-container">
-          {history.map((item) => {
-            // Найти соответствующее предсказание для текущего элемента истории
-            const prediction = userPredictions.find(
-              (pred) => pred.predictionId === item._id
-            );
-
-            return (
-              <div
-                key={item._id}
-                className={`history-item ${getOutcomeClass(item.outcome)}`}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "6px",
-                    width: "100%",
-                  }}
-                >
-                  <p className="history-teams">{item.match}</p>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      margin: "8px 8px",
-                    }}
-                  >
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "6px",
-                      }}
-                    >
-                      <div className="choice-user-history">
-                        Your choice: {item.selectedTeam}
-                      </div>
-                      <div className="result-history-con">
-                        Result: {item.result || "Match is not finished"}
-                      </div>
-                    </div>
-                    {prediction ? (
-                      <div
-                        style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          gap: "6px",
-                        }}
-                      >
-                        <div className="bet-points-history">
-                          Bet points: {prediction.betPoints}
-                        </div>
-                        <div className="total-points-history">
-                          Total: {prediction.betPoints * 2}
-                        </div>
-                      </div>
-                    ) : (
-                      <div>No prediction data found for this match</div>
-                    )}
-                  </div>
-                </div>
+    <div
+      key={item._id}
+      className={`history-item ${getOutcomeClass(item.outcome)}`}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "6px",
+          width: "100%",
+        }}
+      >
+        <p className="history-teams">{item.match}</p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            margin: "8px 8px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "6px",
+            }}
+          >
+            <div className="choice-user-history">
+              Your choice: {item.selectedTeam}
+            </div>
+            <div className="result-history-con">
+              Result: {item.result || "Match is not finished"}
+            </div>
+          </div>
+          {prediction ? (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "6px",
+              }}
+            >
+              <div className="bet-points-history">
+                Bet points: {prediction.betPoints}
               </div>
-            );
-          })}
+              <div className="total-points-history">
+                Total: {prediction.betPoints * 2}
+              </div>
+            </div>
+          ) : (
+            <div>No prediction data found for this match</div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
